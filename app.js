1baa0b98cde2dbd227e2753f5bfad2abcb05a1b6
Leonie10 <leonie.10@hotmail.fr>
2020-11-01 20:45:25 +0100

    Go to step 2

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 let drapeWrap = document.querySelector('.drape');
-
+let textDrape = document.querySelectorAll('.drape-text');
 drapeWrap.addEventListener('click', function () {
     drapeWrap.classList.toggle('drapeUp');
+    for(i = 0; i < textDrape.length; i++){
+        textDrape[i].classList.toggle('drape-textUp');
+
+    }
 })
