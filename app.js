758cd5a6088ee0e909ab4b56857459e37b5d7aaf
Leonie10 <leonie.10@hotmail.fr>
2020-11-02 16:34:11 +0100

    add slideron home page

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+
+// home loading page
 let drapeWrap = document.querySelector('.drape');
 let textDrape = document.querySelectorAll('.drape-text');
-let loaded = document.querySelector('.loaded');
+let homePage = document.querySelector('.homePage');
+let sliderList = document.querySelectorAll('.sliderList');
+
+// home page
 
 drapeWrap.addEventListener('click', function () {
     drapeWrap.classList.toggle('drapeUp');
@@ -8,5 +13,16 @@ drapeWrap.addEventListener('click', function () {
         textDrape[i].style.display = 'none';
     }
     
-    loaded.style.display = 'block';
+    homePage.style.display = 'block';
 })
+
+// diapo picture 
+// quand on passe sur le premier lien on voit la premiere photo
+const pictures = ['vietnam.jpg', ]
+
+sliderList.forEach(li => {
+    li.addEventListener('mouseover' function () {
+        for( i = 0; i < )
+    })
+    
+});
