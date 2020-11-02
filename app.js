06d6c2b26bf694269ef4f59e764a37e736b0f48d
Leonie10 <leonie.10@hotmail.fr>
2020-11-02 09:12:59 +0100

    Begin the first page with slider

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 let drapeWrap = document.querySelector('.drape');
 let textDrape = document.querySelectorAll('.drape-text');
+let loaded = document.querySelector('.loaded');
+
 drapeWrap.addEventListener('click', function () {
     drapeWrap.classList.toggle('drapeUp');
     for(i = 0; i < textDrape.length; i++){
-        textDrape[i].classList.toggle('drape-textUp');
-
+        textDrape[i].style.display = 'none';
     }
+    
+    loaded.style.display = 'block';
 })
