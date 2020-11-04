20ef62727757b57b3bd7b2b96a96458238a013f6
Leonie10 <leonie.10@hotmail.fr>
2020-11-04 12:36:04 +0100

    finish animation slider drape open and close

--- app.js
+++ app.js
@@ -7,31 +7,30 @@ let sliderLi = document.querySelectorAll('.slider-li');
 let mouseOverUl;
 let drapeClass;
 let drapeClassOff = 'slider-drape';
-let drapeClassOn = drapeClassOff + " slider-drape--on";
+let drapeOpenClass = drapeClassOff + " slider-drape--open";
+let drapeCloseClass = drapeClassOff + " slider-drape--close";
 let mouseEvent = '';
 
 function isMouseOver () {
     if(mouseEvent === 'mouseover'){
-        drapeClass = drapeClassOn;
+        drapeClass = drapeOpenClass;
     } else if (mouseEvent === 'mouseout'){
-        drapeClass = drapeClassOff;
+        drapeClass = drapeCloseClass;
     }
     return sliderDrape.classList = drapeClass;
 }
-console.log(sliderUl);
 
 body.addEventListener('mousemove', function (e) {
     if(e.target.classList[0] === 'slider-li'){
-        console.log('true');
         mouseOverUl = true;
         mouseEvent = 'mouseover';
+        isMouseOver();
     } else if(e.target.classList[0] !== 'slider-li'){
         mouseOverUl = false;
         mouseEvent = 'mouseout';
-    }
-    sliderUl.addEventListener( mouseEvent, function () {
         isMouseOver();
-    })
+    }
+    
     console.log(sliderDrape);
 } );
 
