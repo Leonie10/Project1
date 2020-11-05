d85eb6472fa29b11e7ce811431775d143f5f0b9c
Leonie10 <leonie.10@hotmail.fr>
2020-11-05 17:43:45 +0100

    finish slider animation
    finish first part

--- app.js
+++ app.js
@@ -14,52 +14,72 @@ let drapeOpenClass = drapeClassOff + " slider-drape--open";
 let drapeCloseClass = drapeClassOff + " slider-drape--close";
 let mouseEvent = '';
 
-let oldLi;
-let currentLi;
 
-// QUAND MOUSE EST SUR 1 LI ALORS ANIMATION
-// SI MOUSE EST SUR UNE LI DIFFERENTE DONC ELLE BOUGE 
-// ON ENLEVE LA CLASS GROW DE SLIDER IMAGE 
 
-function isMouseOver () {
+
+
+function isMouseOver (li) {
     if(mouseEvent === 'mouseover'){
         drapeClass = drapeOpenClass;
     } else if (mouseEvent === 'mouseout'){
         drapeClass = drapeCloseClass;
-        removeGrowImage();
-    
     }
+   
     return sliderDrape.classList = drapeClass;
 }
 
+
+
 function removeGrowImage () {
-    for(i = 0; i < sliderImg.length; i++){
+    for(let i = 0; i < sliderImg.length; i++){
         sliderImg[i].classList.remove('slider-image--grow');
     }
-    return true;
+}
+
+
+
+function lastLiGrowRemove (target) {
+    if(sliderDrape.classList[1] == 'slider-drape--close'){
+        for(let k = 0; k < sliderImg.length; k++){
+            if(sliderImg[k].classList[1]){
+                setTimeout(function (){
+                sliderImg[k].classList.remove('slider-image--grow');
+                }, 800);
+                }else if(!sliderImg[k].classList[1]){
+                continue;
+            }else{
+                return;
+            }
+        }
+    } else{
+        return;
+    }
 }
 
 function liIsOver (li) {
     removeGrowImage();
     for(x = 0; x < sliderLi.length; x++){
-            if(li == sliderLi[x]){
-                sliderImg[x].classList.add('slider-image--grow');
-            }else{
-                continue;
-            }
+        if(li == sliderLi[x]){
+            sliderImg[x].classList.add('slider-image--grow');
+        }else{
+            continue;
         }
+    }
 }
 
+
 body.addEventListener('mousemove', function (e) {
+    lastLiGrowRemove(e.target);
     if(e.target.classList[0] === 'slider-li'){
         mouseOverUl = true;
         mouseEvent = 'mouseover';
-        isMouseOver();
+        isMouseOver(e.target);
         liIsOver(e.target);
     } else if(e.target.classList[0] !== 'slider-li'){
         mouseOverUl = false;
         mouseEvent = 'mouseout';
         isMouseOver();
+
     }
     
 } );
